b27028df3264b9469dafcbd84d13f789f436a515
hei thavon <thawonptml@gmail.com>
2025-12-07 17:13:17 +0700

    "Fix base path for GitHub Pages deployment"

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/web-app-project/', // <-- IMPORTANT // <-- your repo name
+  base: '/multishooter-pc-game/', // <-- IMPORTANT // <-- your repo name
 })
