67f114647c7d49e11a3eca39765c552736913fa6
hei thavon <thawonptml@gmail.com>
2025-12-07 17:18:56 +0700

    Remove all files from repository

--- src/components/PageContainer.jsx
+++
@@ -1,7 +0,0 @@
-export default function PageContainer({ children, className = "" }) {
-  return (
-    <div className={`page-container page-switch ${className}`}>
-      {children}
-    </div>
-  );
-}
